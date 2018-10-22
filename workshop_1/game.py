7a069124b41a4da2ac609b04fb193012a03a16d6
mineyan <arminegevorgyan1993@gmai.com>
2018-10-22 16:10:09 +0400

    soved partly

--- workshop_1/game.py
+++ workshop_1/game.py
@@ -4,3 +4,30 @@
  # 2.If no digital was entered, show appropriate messageand wait for new input
  # 3. If entered number greater than generated one, showappropriate message
  # 4.If entered number lower than generated one, showappropriate message
+
+import random
+def main():
+    guessingNum = random.randint(0, 20)
+    num = input("Enter an integer from 0 to 20:")
+    print(guessingNum)
+
+    while num:
+        if num.isdigit():
+            if int(num) > guessingNum:
+                print ("number is greater")
+                num = input("Enter an integer from 0 to 20:")
+            elif int(num) < guessingNum:
+                print ("number is lower")
+                num = input("Enter an integer from 0 to 20:")
+            else:
+                print("ok")
+                break
+        else:
+            if num == "exit":
+                break
+            else:
+                print ("not corrct, try to enter digital number!!!")
+                num = input("Enter an integer from 0 to 20:")
+
+
+if __name__ == '__main__': main()
