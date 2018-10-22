5aa9061e4c5d794c47f5c09128126abb36df24a9
mineyan <arminegevorgyan1993@gmai.com>
2018-10-22 16:12:37 +0400

    solved partly

--- workshop_1/game.py
+++ workshop_1/game.py
@@ -1,9 +1,9 @@
  # 2.  Let’s play “Guess The Number Game”.
  # Generate randominteger in range (0,20) and then try to guess it with console input.
  # 1.If “” or “exit” entered, program will exit
- # 2.If no digital was entered, show appropriate messageand wait for new input
- # 3. If entered number greater than generated one, showappropriate message
- # 4.If entered number lower than generated one, showappropriate message
+ # 2.If no digital was entered, show appropriate message and wait for new input
+ # 3.If entered number greater than generated one, show appropriate message
+ # 4.If entered number lower than generated one, show appropriate message
 
 import random
 def main():
