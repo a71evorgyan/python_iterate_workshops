ab64e39594a2743a87b45ae8ad1bf3cc14d0b2aa
mineyan <arminegevorgyan1993@gmai.com>
2018-10-22 17:28:52 +0400

    solved partly

--- workshop_1/zen.py
+++ workshop_1/zen.py
@@ -1,2 +1,26 @@
  # 3. Export list of 3 most common words in Python Zen,
  #  whereeach item of list looks like (‘word’, frequency)
+
+
+def main():
+    file = open("zen.txt", "r")
+    data = file.read()
+    # print (data)
+    arr = data.split("\n")
+    str = ' '.join(arr)
+
+    array = str.split(' ')
+    # print (array)
+
+    count = 0
+    d = {}
+    n = len(array)
+    for i in range(n):
+        for j in range(n):
+            if(array[j] == array[i]):
+                count = count + 1
+        d[array[i]] = count
+        count = 0
+    print(sorted(d.items(), key=lambda x: x[n]))
+
+if __name__ == '__main__': main()
